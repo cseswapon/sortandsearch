df9da826b751a2c98e91eeaaba2c74b3b1628773
cseswapon <cseswaponsaha@gmail.com>
2023-09-30 23:05:15 +0600

    [ADD] - linear search

--- Linear Search/main.c
+++ Linear Search/main.c
@@ -1,5 +1,27 @@
-#include<stdio.h>
-int main(){
-    printf("Hello world");
+#include <stdio.h>
+
+int main()
+{
+    int size;
+
+    printf("Define Array Size: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter Your Input Numbers:\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int length = size;
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+
     return 0;
 }
